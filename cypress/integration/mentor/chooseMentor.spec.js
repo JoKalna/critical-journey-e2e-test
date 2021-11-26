715acfbc0d9d02f89b45c4c42a57924a8d2b099b
Jo Kalnavarna <madara.k@hotmail.co.uk>
2021-11-26 13:07:47 +0000

    initial commit to add cypress to the project

---
+++ cypress/integration/mentor/chooseMentor.spec.js
@@ -0,0 +1,10 @@
+describe('example to-do app', () => {
+  beforeEach(() => {
+    // beforeEach function used so that it runs before each test
+  })
+
+  it('should visit students page', () => {
+    cy.visit('https://unibuddy.co/pwa/demo-university/buddies/students')
+  })
+
+})
