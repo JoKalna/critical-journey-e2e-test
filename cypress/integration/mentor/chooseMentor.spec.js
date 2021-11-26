2fd98d6013fe4a2ba4f2b3d7f9a71277ed560ad7
Jo Kalnavarna <madara.k@hotmail.co.uk>
2021-11-26 19:03:44 +0000

    add choose mentor test and pre registration test

--- cypress/integration/mentor/chooseMentor.spec.js
+++ cypress/integration/mentor/chooseMentor.spec.js
@@ -1,35 +1,46 @@
 describe('Student choosing a mentor to chat with', () => {
-    beforeEach(() => {
+    before(() => {
         // beforeEach function used so that it runs before each test
+        cy.visit('/')
+        cy.get('#onetrust-accept-btn-handler').click()
     })
 
-    it('should visit students page', () => {
-        cy.visit('https://unibuddy.co/pwa/demo-university/buddies/students')
+    // check if grid has at least one child visibile for the selected option
+    function verifyGrid() {
+        cy.get('._91w > :nth-child(1)')
+            .should('be.visible')
+    }
+
+    it('should select Area of Study, Levels and Country', () => {
+        // select Area Of Study
+        cy.get('#mentor-filter-degree > div > div.css-1hwfws3')
+            .click()
+        cy.get('#react-select-2-option-5')
+            .click({ force: true })
+        // verify if selected Area of Study is displayed as label                                       
+        cy.get('#remove-label')
+            .should('exist');
+        verifyGrid();
+        // select Degree Level  
+        cy.get('#mentor-filter-degree_level > .css-1ns25le > .css-1hwfws3')
+            .click()
+        cy.get('#react-select-3-option-0')
+            .click({ force: true })
+        cy.get('._UcR > :nth-child(2)')
+            .should('exist');
+        verifyGrid()
+        // select Country    
+        cy.get('#mentor-filter-country > .css-1ns25le > .css-1hwfws3')
+            .click()
+        cy.get('#react-select-4-option-0')
+            .click({ force: true })
+        cy.get('._UcR > :nth-child(3)')
+            .should('exist');
+        verifyGrid()
+        cy.get(':nth-child(1) > ._1Tj0')
+        // verify and click on chat button
+        cy.get(':nth-child(1) > ._2qHr > ._1lC1')
+            .should('contain', 'Chat with')
+            .click({ force: true })
     })
-    
-    it('should select Area of Study', () => {
-
-        // verify that everytime you select Area of Studythe grid updates and has at least 1 length. 
-        // verify that remove-label has been updated 
-
-    })
-    it('should select Levels', () => {
-
-        // verify that everytime you select Level, the grid updates and has at least 1 length. 
-        // verify that remove-label has been updated 
-
-    })
-    it('should select country', () => {
-
-        // verify that everytime you select country, the grid updates and has at least 1 length. 
-        // verify that remove-label has been updated 
-
-    })
-
-    it('should have Chat with ... button is presented', () => {
-       // button match 'Chat with
-       // click the button
-       // verify that the once the button is pressed href matches the buddy postion=1   
-    })
-
 })
--- cypress/integration/pre_registration_chat_box/chat.spec.js
+++ cypress/integration/pre_registration_chat_box/chat.spec.js
@@ -1,32 +1,24 @@
+beforeEach(() => {
+    // best would be reuse the session from previous test in chooseMentor.spec.js 
+    cy.visit('/inbox/pre/609e54cf6eca6d2bbb0ff086?buddyPosition=1')
+    cy.get('#onetrust-accept-btn-handler').click()
+})
 describe('Student presented with chat screen pre-registrataion process', () => {
-    beforeEach(() => {
-        // beforeEach function used so that it runs before each test
-    })
-    
     it('should have chat-container with profile name and avatar ', () => {
-
-        // verify that data-test-id is present
-        // verify that person has an avatar with image-test-id 
-
-    })
-    it('should have Create your account wording', () => {
-
-        // verify that Create your account is present
-        // verify that start chatting now is present
-
+        cy.get('#dummy-chat > div > div > div > div > div._1IE0 > div._3wKi > div > div >a')
+            .should('have.attr', 'href', '/pwa/demo-university/buddies/students/609e54cf6eca6d2bbb0ff086?buddyPosition=1')
     })
     it('should have Send button state disabled for none logged in users', () => {
 
-        // verify that chat-input-send-Desktop is disabled  that includes type here 
-        // verify that Send button is disabled
-
+        cy.get('[data-test-id="chat-input-send-Desktop"]')
+            .should('be.disabled')
+    })
+    // assert href?
+    it('should have `Start chatting` now button present and user can click on it', () => {
+        cy.get('#register')
+            .should('contain', 'Start chatting now')
+            .click()
     })
 
-    it('should have `Start chatting` now button present', () => {
-       // button match 'Start chatting
-       // click the button
-       // verify that the once the button is pressed href matches the buddy postion=1 and auth/register?buddyPosition=1
-       //verify that it says Chat to your future maybe? 
-    })
 
 })
