f2d3a79e7b0f5c984461dbc9ddc0c249e7f6e74e
Jo Kalnavarna <madara.k@hotmail.co.uk>
2021-11-28 19:03:02 +0000

    refactor pre-reg chat box test

--- cypress/integration/pre_registration_chat_box/chat.spec.js
+++ cypress/integration/pre_registration_chat_box/chat.spec.js
@@ -1,11 +1,11 @@
 beforeEach(() => {
     // best would be reuse the accessToken from previous test in chooseMentor.spec.js 
-    cy.visit('/inbox/pre/609e54cf6eca6d2bbb0ff086?buddyPosition=1')
+    cy.visit('/pwa/demo-university/inbox/pre/6123c95f97f1a2234816332d?buddyPosition=1')
     cy.get('#onetrust-accept-btn-handler').click()
 })
-describe('Student presented with the chat screen pre-reg process', () => {
+describe('Student presented with the chat screen during pre-reg process', () => {
 
-    it('should have Create your account displayed in the chat box', () => {
+    it('should have `Create your account` displayed in the chat container', () => {
         cy.get('[data-test-id="chat-container"] div[class^=tzcn]')
             .contains('Create your account')
     })
@@ -13,6 +13,9 @@ describe('Student presented with the chat screen pre-reg process', () => {
     it('should have Send button state disabled for none logged in users', () => {
         cy.get('[data-test-id="chat-input-send-Desktop"]')
             .should('be.disabled')
+        cy.get('#login-tab')
+            .invoke('attr', 'href')
+            .should('contain', 'auth/register')
     })
 
     it('should have `Start chatting` now button present and user can click on it', () => {
@@ -21,6 +24,5 @@ describe('Student presented with the chat screen pre-reg process', () => {
             .click()
         cy.url()
             .should('include', 'pwa/demo-university/auth/register?buddyPosition')
-
     })
 })
